83a016f5c4c53a64fcc7bbe951cf7e2c59dac7ac
Kirill Galenko <kirill.galenko@awave.com>
2022-01-13 16:46:18 +0200

    minor changes to reset mail

--- wp-content/themes/ddapi/inc/reset-password.php
+++ wp-content/themes/ddapi/inc/reset-password.php
@@ -2,31 +2,28 @@
 function runRetrivePassword($data){
     global $wpdb, $wp_hasher;
     $user_data = get_user_by('email',  $data['email']);
-    if (!$user_data) return array('result' => false);
+    if (!$user_data) return array('result' => true);
 
     do_action('lostpassword_post');
     $user_login = $user_data->user_login;
     $user_email = $user_data->user_email;
     $key = get_password_reset_key($user_data);
     $message = __('Someone requested that the password be reset for the following account:') . "\r\n\r\n";
-    $message .= network_home_url('/') . "\r\n\r\n";
-    $message .= sprintf(__('Username: %s'), $user_login) . "\r\n\r\n";
+    $message .= "dailyDo.lv" . "\r\n";
+    $message .= sprintf(__('Account email: %s'), $user_email) . "\r\n\r\n";
     $message .= __('If this was a mistake, just ignore this email and nothing will happen.') . "\r\n\r\n";
-    $message .= __('Dude, To reset your password, visit the following address:') . "\r\n\r\n";
+    $message .= __('To reset your password, visit the following address:') . "\r\n\r\n";
     // $message .= network_site_url("wp-login.php?action=rp&to=$key&em=" . rawurlencode($user_login), 'login');
 
     $message .= '/user/reset/?em='.rawurlencode($user_email).'&to='.$key;
 
-    if (is_multisite())
-        $blogname = $GLOBALS['current_site']->site_name;
-    else
-        $blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
 
-    $title = sprintf(__('[%s] Password Reset'), $blogname);
+
+    $title = __('DailyDo.lv Password Reset');
     $title = apply_filters('retrieve_password_title', $title);
     $message = apply_filters('retrieve_password_message', $message, $key);
-    if ($message && !wp_mail($user_email, $title, $message))
-        wp_die(__('The e-mail could not be sent.') . "<br />\n" . __('Possible reason: your host may have disabled the mail() function...'));
+
+    wp_mail($user_email, $title, $message);
 
     return array('result' => true);
 }
