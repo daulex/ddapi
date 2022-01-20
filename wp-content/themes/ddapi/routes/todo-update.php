f9bf900e4e66f7faac855fedf6ac3c85c7eb2a09
Kirill Galenko <kirill.galenko@awave.com>
2022-01-20 15:11:54 +0200

    todo/update now updates today's todo, for the correct user and with the correct today todo label

--- wp-content/themes/ddapi/routes/todo-update.php
+++ wp-content/themes/ddapi/routes/todo-update.php
@@ -1,11 +1,23 @@
 <?php
 function ddapi_todo_update( $data ): int {
-	$parsed = json_decode($data->get_body());
-	$new = maybe_serialize($parsed->todos);
 
-	update_post_meta( 6, 'todo', $new);
+	$user = wp_get_current_user();
+	if($user->ID === 0) return 403;
 
-	return 200;
+	if(!isset($data['id'])){
+
+		$key = get_todo_label();
+		$user_todo_store = get_user_store($user->ID, "todo");
+		$parsed = json_decode($data->get_body());
+
+		$new = maybe_serialize($parsed->todos);
+
+		update_post_meta( $user_todo_store, $key, $new);
+
+		return 200;
+	}
+
+	return 403;
 }
 
 add_action( 'rest_api_init', function () {
