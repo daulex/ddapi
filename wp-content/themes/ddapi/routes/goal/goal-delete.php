0861ef9fb9ba9d91115c2a4e3ec809b034f61468
Kirill Galenko <kirill.galenko@awave.com>
2022-03-10 19:40:26 +0200

    minor delete fix

--- wp-content/themes/ddapi/routes/goal/goal-delete.php
+++ wp-content/themes/ddapi/routes/goal/goal-delete.php
@@ -1,13 +1,13 @@
 <?php
 
-function ddapi_goal_delete( $data ) {
+function ddapi_goal_delete( $data ): int {
 	$post = get_post(intval($data['id']));
 	if(!$post) return 404;
 
 	$user = wp_get_current_user();
 	if($user->ID === 0 || intval($post->post_author) !== $user->ID) return 403;
 
-	return !!wp_delete_post($post->ID, true);
+	return !!wp_delete_post($post->ID, true) === true ? 200 : 500;
 }
 
 add_action( 'rest_api_init', function () {
